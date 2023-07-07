2c7f3c1095a02097c37dd2204500373892469afa
sumant1512 <sumantmishra511@gmail.com>
2023-07-07 22:52:27 +0530

    light header completed

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { IImage } from '../interfaces/image.interface';
+import { IUserInfo } from 'src/app/store/user-info/interfaces/user-info.interface';
 
 @Component({
   selector: 'app-header',
@@ -7,6 +9,20 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  cartImage: IImage = {
+    imgUrl: './../../../assets/cart.png',
+    imgAlt: 'Cart',
+  };
+
+  userInfo: IUserInfo = {
+    userImage: {
+      imgUrl: './../../../assets/userImg.png',
+      imgAlt: 'Cart',
+    },
+    firstName: 'Sumant',
+    lastName: 'Mishra',
+  };
+
   constructor(private modalService: NgbModal) {}
 
   public open(modal: any): void {
---
+++ src/app/store/user-info/interfaces/user-info.interface.ts
@@ -0,0 +1,7 @@
+import { IImage } from 'src/app/shared/interfaces/image.interface';
+
+export interface IUserInfo {
+  userImage: IImage;
+  firstName: string;
+  lastName: string;
+}
