a65ce56f7d5e88577a73ae9148f601f29c720416
sumant1512 <sumantmishra511@gmail.com>
2023-07-08 03:34:42 +0530

    footer theme added

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { IFooterUrlList } from '../interfaces/footer-url-list.interface';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
-
+  footerUrlList: Array<IFooterUrlList> = [
+    {
+      headerName: 'Responses',
+      urlList: [
+        { name: 'How it Works', url: '' },
+        { name: 'Gurantee', url: '' },
+        { name: 'Security', url: '' },
+        { name: 'Pricing', url: '' },
+      ],
+    },
+    {
+      headerName: 'Company',
+      urlList: [
+        { name: 'About Us', url: '' },
+        { name: 'Prices', url: '' },
+        { name: 'Blog', url: '' },
+        { name: 'License', url: '' },
+      ],
+    },
+    {
+      headerName: 'Support',
+      urlList: [
+        { name: 'Getting Started', url: '' },
+        { name: 'Help Center', url: '' },
+        { name: 'Report a bug', url: '' },
+        { name: 'Contact us', url: '' },
+      ],
+    },
+  ];
 }
---
+++ src/app/shared/interfaces/footer-url-list.interface.ts
@@ -0,0 +1,9 @@
+export interface IFooterUrlList {
+  headerName: string;
+  urlList: Array<IUrlList>;
+}
+
+export interface IUrlList {
+  name: string;
+  url: string;
+}
