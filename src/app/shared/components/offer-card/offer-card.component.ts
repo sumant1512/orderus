336e6303ba705b1a4f19a73f74127c9f73298e6a
sumant1512 <sumantmishra511@gmail.com>
2023-07-08 22:42:53 +0530

    offer card added

--- src/app/shared/components/offer-card/offer-card.component.ts
+++ src/app/shared/components/offer-card/offer-card.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IOfferCardDetails } from '../../interfaces/offer-card.interface';
 
 @Component({
   selector: 'app-offer-card',
   templateUrl: './offer-card.component.html',
-  styleUrls: ['./offer-card.component.scss']
+  styleUrls: ['./offer-card.component.scss'],
 })
 export class OfferCardComponent {
-
+  @Input() cardDetails!: IOfferCardDetails;
 }
---
+++ src/app/shared/interfaces/offer-card.interface.ts
@@ -0,0 +1,8 @@
+export interface IOfferCardDetails {
+  offerCardImage: string;
+  offerCardImageAlt: string;
+  offerCardHeader: string;
+  offer?: string;
+  typeLabel: string;
+  type: string;
+}
