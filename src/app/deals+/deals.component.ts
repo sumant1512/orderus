d561f91ba0d6652fff956c437afc475be5a945f6
sumant1512 <sumantmishra511@gmail.com>
2023-07-09 19:33:44 +0530

    partial card added

--- src/app/deals+/deals.component.ts
+++ src/app/deals+/deals.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
+import { IDealCardDetail } from '../shared/interfaces/deal-card.interface';
+import { EDealBtn } from '../shared/enum/deal-btn.enum';
 
 @Component({
   selector: 'app-deals',
   templateUrl: './deals.component.html',
-  styleUrls: ['./deals.component.scss']
+  styleUrls: ['./deals.component.scss'],
 })
 export class DealsComponent {
-
+  dealsList: Array<IDealCardDetail> = [
+    {
+      btnType: EDealBtn.ACTIVE,
+      imgUrl: './../../assets/deals-card/deal-1.jpg',
+      imgAlt: 'sandwich',
+      name: '20% off on Vegan sandwiched every Saturday this Summer',
+      validity: {
+        validityStartsFrom: new Date().toISOString(),
+        validityEndsOn: new Date().toDateString(),
+        validityInfo: 'Saturday Only',
+      },
+      restaurantType: 'Vegfood',
+    },
+    {
+      btnType: EDealBtn.SCHEDULED,
+      imgUrl: './../../assets/deals-card/deal-1.jpg',
+      imgAlt: 'sandwich',
+      name: '20% off on Vegan sandwiched every Saturday this Summer',
+      validity: {
+        validityStartsFrom: new Date().toISOString(),
+        validityEndsOn: new Date().toDateString(),
+      },
+      restaurantType: 'Vegfood',
+    },
+    {
+      btnType: EDealBtn.EXPIRED,
+      imgUrl: './../../assets/deals-card/deal-1.jpg',
+      imgAlt: 'sandwich',
+      name: '20% off on Vegan sandwiched every Saturday this Summer',
+      validity: {
+        validityStartsFrom: new Date().toISOString(),
+        validityEndsOn: new Date().toDateString(),
+      },
+      restaurantType: 'Vegfood',
+    },
+  ];
 }
--- src/app/deals+/deals.module.ts
+++ src/app/deals+/deals.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { DealsRoutingModule } from './deals-routing.module';
 import { DealsComponent } from './deals.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [DealsComponent],
-  imports: [CommonModule, DealsRoutingModule],
+  imports: [CommonModule, DealsRoutingModule, SharedModule],
 })
 export class DealsModule {}
