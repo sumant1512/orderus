1129451e5e3ceb38627856e5c408e572f7a47157
sumant1512 <sumantmishra511@gmail.com>
2023-07-16 22:37:54 +0530

    rating header issue fix for mobile

--- src/app/shared/components/restaurant-card/restaurant-card.component.ts
+++ src/app/shared/components/restaurant-card/restaurant-card.component.ts
@@ -10,8 +10,8 @@ export class RestaurantCardComponent {
   @Input() restaurantCardDetails!: IRestaurantCardDetail;
   @Output() onCardClick = new EventEmitter<number>();
 
-  greyCartImage = './../../../../assets/restaurant-card/cart-grey.png';
-  blueCartImage = './../../../../assets/restaurant-card/cart-blue.png';
+  greyCartImage = './../../../../assets/restaurant-card/cart-grey.svg';
+  blueCartImage = './../../../../assets/restaurant-card/cart-blue.svg';
 
   cardClick(id: number): void {
     if (id) {
