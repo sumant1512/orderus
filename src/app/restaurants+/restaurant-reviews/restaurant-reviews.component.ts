3409139da9b81774eae1bf0c6519ab71d7e022c1
sumant1512 <sumantmishra511@gmail.com>
2023-07-17 13:02:17 +0530

    review and rating changes done

--- src/app/restaurants+/restaurant-reviews/restaurant-reviews.component.ts
+++ src/app/restaurants+/restaurant-reviews/restaurant-reviews.component.ts
@@ -8,15 +8,29 @@ import { IRestaurantReviews } from 'src/app/store/restaurant-details/interfaces/
   styleUrls: ['./restaurant-reviews.component.scss'],
 })
 export class RestaurantReviewsComponent {
-  @Input() reviews!: IRestaurantReviews[];
-  selectedRating = 3;
-
+  reviewList!: Array<IRestaurantReviews>;
+  selectedRating: number = 0;
+  votesCount: number = 0;
   dropDownList: Array<IDropdown> = [
     { id: 1, name: 'Newest First' },
     { id: 2, name: 'Oldest First' },
   ];
 
+  @Input() set reviews(value: Array<IRestaurantReviews>) {
+    this.reviewList = value;
+    this.votesCount = this.reviewList.length;
+    this.setOverAllRating(this.reviewList);
+  }
+
   reviewBtnClick(event: boolean): void {
     console.log(event);
+  }
+
+  setOverAllRating(allRating: Array<IRestaurantReviews>): void {
+    const sumOfRatings = allRating.reduce(
+      (sum, review) => sum + review.rating,
+      0
+    );
+    this.selectedRating = sumOfRatings / allRating.length;
   }
 }
