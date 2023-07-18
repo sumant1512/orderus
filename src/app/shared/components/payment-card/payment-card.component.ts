0724445e66ec7a28c2a195ad50cd336a584fc7ce
sumant1512 <sumantmishra511@gmail.com>
2023-07-18 23:33:48 +0530

    payment card added basic

--- src/app/shared/components/payment-card/payment-card.component.ts
+++ src/app/shared/components/payment-card/payment-card.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IPayment } from 'src/app/store/user-info/interfaces/user-info.interface';
 
 @Component({
   selector: 'app-payment-card',
   templateUrl: './payment-card.component.html',
-  styleUrls: ['./payment-card.component.scss']
+  styleUrls: ['./payment-card.component.scss'],
 })
 export class PaymentCardComponent {
-
+  @Input() payment!: IPayment;
 }
