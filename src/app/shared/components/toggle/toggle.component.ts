5a33795cd07a1fabe7a504ffea88d38970037bf0
sumant1512 <sumantmishra511@gmail.com>
2023-07-20 00:53:28 +0530

    pick up and delivery option adde don cart

--- src/app/shared/components/toggle/toggle.component.ts
+++ src/app/shared/components/toggle/toggle.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-toggle',
   templateUrl: './toggle.component.html',
-  styleUrls: ['./toggle.component.scss']
+  styleUrls: ['./toggle.component.scss'],
 })
 export class ToggleComponent {
+  @Output() selectedSection = new EventEmitter<string>();
 
+  emitSelectedSection(section: string): void {
+    this.selectedSection.emit(section);
+  }
 }
