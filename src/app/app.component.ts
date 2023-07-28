0fc52e1daa52335cab81718d9e8a73db6f1179f8
sumant1512 <sumantmishra511@gmail.com>
2023-07-29 03:18:05 +0530

    added login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ThemeService } from './shared/services/theme/theme.service';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { APP_ROUTES } from './shared/constants/app-routes.constants';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'orderus';
+  isHeaderFooterActive = true;
 
-  constructor(private themeService: ThemeService) {
+  constructor(
+    private themeService: ThemeService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
     this.themeService.setThemeOnStart();
+  }
+
+  ngOnInit(): void {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.isHeaderFooterActive = [
+          APP_ROUTES.LOGIN,
+          APP_ROUTES.REGISTRATION,
+          APP_ROUTES.FORGET_PASSWORD,
+        ].includes(this.route.snapshot.firstChild?.routeConfig?.path || '');
+      });
   }
 
   toggleTheme(): void {
--- src/app/login+/login.component.ts
+++ src/app/login+/login.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { LoginForm } from './login.form';
+import { Router } from '@angular/router';
+import { APP_ROUTES } from '../shared/constants/app-routes.constants';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  loginForm = LoginForm();
+  appRoutes = APP_ROUTES;
 
+  constructor(private router: Router) {}
+
+  navigateToPage(routePath: string): void {
+    this.router.navigate([routePath]);
+  }
 }
