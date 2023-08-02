8548057bfd1f1b977497a318e93d7a1b57f72cbc
sumant1512 <sumantmishra511@gmail.com>
2023-08-02 19:54:44 +0530

    login form changes

--- src/app/Personal/login+/login.form.ts
+++ src/app/Personal/login+/login.form.ts
@@ -2,8 +2,8 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 export function LoginForm(): FormGroup {
   return new FormGroup({
-    email: new FormControl('', [Validators.required]),
-    password: new FormControl('', [Validators.required]),
-    keepSignedIn: new FormControl(''),
+    email: new FormControl('test@gmail.com', [Validators.required]),
+    password: new FormControl('123456', [Validators.required]),
+    keepSignedIn: new FormControl(true),
   });
 }
