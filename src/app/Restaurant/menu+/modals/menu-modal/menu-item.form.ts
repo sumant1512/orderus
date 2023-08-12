c2088b68971bedc9addfce1bb052d28bddb8cbf8
sumant1512 <sumantmishra511@gmail.com>
2023-08-12 22:05:01 +0530

    menu form added for add

--- src/app/Restaurant/menu+/modals/menu-modal/menu-item.form.ts
+++ src/app/Restaurant/menu+/modals/menu-modal/menu-item.form.ts
@@ -3,5 +3,8 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export function MenuItemForm(): FormGroup {
   return new FormGroup({
     name: new FormControl('', [Validators.required]),
+    price: new FormControl('', [Validators.required]),
+    description: new FormControl('', [Validators.required]),
+    ingredeints: new FormControl('', [Validators.required]),
   });
 }
